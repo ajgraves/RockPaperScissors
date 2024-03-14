ad3ba5bc4d94faf283627989905d097df6af6e4e
Aaron Graves <ajgraves@ajgraves.com>
2024-03-13 20:28:09 -0500

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Aaron Graves on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
